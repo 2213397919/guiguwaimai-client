584d52fffe3490316f093594b77505d6737ee5ce
zhangshun <2213397918@qq.com>
2018-12-22 16:55:31 +0800

    完成了api编写,配置了服务器代理

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,14 @@
-//定义请求模块
-import axios from 'axios'
-export default function ajax (url,data={},method='GET') {
 
-}
+/*
+包含n个接口请求函数的模块
+每个函数返回promise
+ */
+import ajax from './ajax'
+const BASE = '/api'  // 开发环境下需要使用代理帮我们转发请求
+
+// [1、根据经纬度获取位置详情](#1根据经纬度获取位置详情)<br/>
+export const reqAddress = (longitude, latitude) => ajax(BASE + `/position/${latitude},${longitude}`)
+// [2、获取食品分类列表](#2获取食品分类列表)<br/>
+export const reqCategorys = () => ajax(BASE + '/index_category')
+// [3、根据经纬度获取商铺列表](#3根据经纬度获取商铺列表)<br/>
+export const reqShops = (longitude, latitude) => ajax(BASE + '/shops', {longitude, latitude})
